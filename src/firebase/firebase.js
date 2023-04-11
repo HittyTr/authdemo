96fa456decb22ae3267b2c4d139910804eb6a1ca
Koray Akpinar <akpinarkoray@gmail.com>
2023-04-11 17:43:22 -0400

    api hiding

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,11 +5,11 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY ,
-  authDomain: "logindemo-5d488.firebaseapp.com",
-  projectId: "logindemo-5d488",
-  storageBucket: "logindemo-5d488.appspot.com",
-  messagingSenderId: "40086703564",
-  appId: "1:40086703564:web:e329ea74bbd3880d006169"
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
